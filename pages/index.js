26188f3e0531d0de03677ec87e2dbabfa4d89426
Harut333 <harutavetisyan0@gmail.com>
2023-02-13 20:06:28 +0400

    commit 13/02

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-// import "../src/scss/layout.scss"
 import Link from "next/link";
 import Header from "../src/Header";
 
 
 export default function Home() {
-  return (
+    return (
         <>
             <Header/>
-
             <div className={"container"}>
-            <div className={"layout"}>
-              <div className={"homepage"}>
-              </div>
+                <div className={"layout"}>
+                    <div className={"homepage"}>
+                    </div>
+                </div>
             </div>
-          </div>
         </>
     )
 }
