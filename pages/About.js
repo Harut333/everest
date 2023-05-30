f7511f74d714707e3f33955532f1c529cefdf872
Harut333 <harutavetisyan0@gmail.com>
2023-05-30 21:48:36 +0400

    initial commit

--- pages/About.js
+++ pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/About.module.scss';
+import styles from '../styles/_about.module.scss';
 
 const About = () => {
     return (
--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/Home.module.scss';
+import styles from '../styles/_home.module.scss';
 
 const Home = () => {
     return (
